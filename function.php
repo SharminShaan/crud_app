8cd0a5a55c0b0932a7b01393f92436570059e282
Sharmin <sharmin.idb.info@gmail.com>
2024-02-14 11:50:40 +0600

    crud app updated file

--- function.php
+++ function.php
@@ -11,10 +11,9 @@ class crudApp
         $dbpass = "";
         $dbname = 'crudapp';
 
-        $this->conn= mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+        $this->conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-        if (!$this->conn)
-        {
+        if (!$this->conn) {
             die("Database Connection Error! !");
         }
     }
@@ -27,23 +26,24 @@ class crudApp
 
         $query = "INSERT INTO DATA_INFO(s_name,s_roll,s_image) VALUE('$s_name',$s_roll,'$s_image')";
 
-        if(mysqli_query($this->conn, $query))
-        {   
-            move_uploaded_file($tmp_name, 'upload/'.$s_image);
+        if (mysqli_query($this->conn, $query)) {
+            move_uploaded_file($tmp_name, 'upload/' . $s_image);
             return "Information Added Successfully";
         }
     }
     public function display_data()
     {
         $query = "SELECT * FROM data_info";
-        if(mysqli_query($this->conn, $query)){$returndata = mysqli_query                  ($this->conn, $query);
+        if (mysqli_query($this->conn, $query)) {
+            $returndata = mysqli_query($this->conn, $query);
             return $returndata;
         }
     }
     public function display_data_by_id($id)
     {
         $query = "SELECT * FROM data_info where id=$id";
-        if(mysqli_query($this->conn, $query)){$returndata = mysqli_query                  ($this->conn, $query);
+        if (mysqli_query($this->conn, $query)) {
+            $returndata = mysqli_query($this->conn, $query);
             $infoData = mysqli_fetch_assoc($returndata);
             return $infoData;
         }
@@ -58,24 +58,35 @@ class crudApp
 
         $query = "UPDATE data_info SET s_name='$s_name',s_roll='$s_roll',s_image='$st_img' WHERE id='$sid'";
 
-        if(mysqli_query($this->conn, $query)){
-            move_uploaded_file($tmp_name, 'upload/'.$st_img);
+        if (mysqli_query($this->conn, $query)) {
+            move_uploaded_file($tmp_name, 'upload/' . $st_img);
             return "Informaation Updated Successfully!";
         }
-
     }
-    public function delete_data($id){
+    public function delete_data($id)
+    {
         $catch_img = "SELECT * from data_info where id=$id";
         $delete_std_info = mysqli_query($this->conn, $catch_img);
         $std_infoDel = mysqli_fetch_assoc($delete_std_info);
         $deleteImg_data = $std_infoDel['s_image'];
         $query = "DELETE from data_info WHERE id=$id";
-        if(mysqli_query($this->conn, $query)){
-            unlink('upload/'.$deleteImg_data);
-           return "Deleted Successfully";
+        if (mysqli_query($this->conn, $query)) {
+            unlink('upload/' . $deleteImg_data);
+            
+
+            header("Location: index.php?message=Delete success!");
         }
     }
+
+    // public function delete_data($id){
+    //     $catch_img = "SELECT * FROM students WHERE id=$id";
+    //     $delete_std_info = mysqli_query($this->conn, $catch_img);
+    //     $std_infoDle = mysqli_fetch_assoc($delete_std_info);
+    //     $deleteImg_data = $std_infoDle['stg_img'];
+    //     $query = "DELETE FROM students WHERE id=$id";
+    //     if(mysqli_query($this->conn, $query)){
+    //         unlink('upload/'.$deleteImg_data);
+    //         return "Deleted Successfully";
+    //     }
+    // }
 }
-
-
-
--- index.php
+++ index.php
@@ -1,18 +1,28 @@
-<?php 
-    include("function.php");
+<?php
+include("function.php");
 
-    $objCrudAdmin = new crudApp();
+$objCrudAdmin = new crudApp();
 
-    if(isset($_POST['add_info'])){
-       $return_msg = $objCrudAdmin->add_data($_POST);
+if (isset($_POST['add_info'])) {
+    $return_msg = $objCrudAdmin->add_data($_POST);
+}
+$data_info = $objCrudAdmin->display_data();
+
+// if(isset($GET['status'])){
+//     if($_GET['status']=='delete'){
+//        $delete_id = $_GET['id'];
+//        $delmsg = $objCrudAdmin->delete_data($delete_id);
+//     }
+// }
+
+if (isset($_GET['status'])) {
+    if ($_GET['status'] = 'delete') {
+        $delete_id = $_GET['id'];
+        $delmsg =  $objCrudAdmin->delete_data($delete_id);
     }
-    $data_info = $objCrudAdmin->display_data();
-    if(isset($GET['status'])){
-        if($_GET['status']='delete'){
-           $delete_id = $_GET['id'];
-           $delmsg = $objCrudAdmin->delete_data($delete_id);
-        }
-    }
+}
+
+
 
 ?>
 
@@ -20,7 +30,8 @@
 
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -29,15 +40,48 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title>CRUD APP</title>
-  </head>
-  <body>
+</head>
+
+<body>
     <div class="container my-4 p-4 shadow">
         <h2><a style="text-decoration: none;" href="index.php">Sample Database</a></h2>
-        <?php if(isset($delmsg)){
-            echo $delmsg;
-        } ?>
+
+        <?php
+
+        // session_start();
+
+        // $msg = $_GET['message'];
+        // if (isset($msg)) {
+
+        //     echo '<p style="background-color: red; padding:5px;"  class="message"> ' . $msg . '</p>';
+        // }
+
+
+        // if (!empty($_SESSION['message'])) {
+        //     $message = $_SESSION['message'];
+
+        //     echo '<p class="message"> ' . $_SESSION['message'] . '</p>';
+        //     unset($_SESSION['message']);
+        // }
+
+        $msg = $_GET['message'];
+        if (isset($msg)) {
+            echo '<p style="background-color: red; padding:5px;"  class="message"> ' . $msg . '</p>';
+            // unset($_SESSION['message']);
+        }else{
+            echo '';
+        }
+
+
+        ?>
+
+
+
+
         <form class="form" action="" method="post" enctype="multipart/form-data">
-            <?php if(isset($return_msg)){echo $return_msg;} ?>
+            <?php if (isset($return_msg)) {
+                echo $return_msg;
+            } ?>
             <input class="form-control mb-2" type="text" name="s_name" placeholder="Enter Name">
             <input class="form-control mb-2" type="number" name="s_roll" placeholder="Enter Roll">
             <label for="s_image">Upload Image</label>
@@ -48,26 +92,33 @@
     <div class="container my-4 p-4 shadow">
         <table class="table table-responsive">
             <thead>
-               <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Roll</th>
-                <th>Image</th>
-                <th>Action</th>
-               </tr> 
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Roll</th>
+                    <th>Image</th>
+                    <th>Action</th>
+                </tr>
             </thead>
             <tbody>
-            <?php while($data=mysqli_fetch_assoc($data_info)){ ?>
-                <tr>
-                    <td><?php echo $data['id']; ?></td>
-                    <td><?php echo $data['s_name']; ?></td>
-                    <td><?php echo $data['s_roll']; ?></td>
-                    <td><img style="height: 100px;" src="upload/<?php echo $data['s_image']; ?>"></td>
-                    <td>
-                        <a class="btn btn-success" href="edit.php?status=edit&&id=<?php echo $data['id']; ?>">Edit</a>
-                        <a class="btn btn-warning" href="?status=delete&&id=<?php echo $data['id']; ?>">Delete</a>
-                    </td>
-                </tr>
+                <?php while ($data = mysqli_fetch_assoc($data_info)) { ?>
+                    <tr>
+                        <td><?php echo $data['id']; ?></td>
+                        <td><?php echo $data['s_name']; ?></td>
+                        <td><?php echo $data['s_roll']; ?></td>
+                        <td><img style="height: 100px;" src="upload/<?php echo $data['s_image']; ?>"></td>
+                        <td>
+                            <a class="btn btn-success" href="edit.php?status=edit&&id=<?php echo $data['id']; ?>">Edit</a>
+
+
+                            <!-- <a class="btn btn-warning" href="?status=delete&&id=<?php echo $data['id']; ?>">Delete</a> -->
+
+                            <a class="btn btn-warning" href="?status=delete&&id=<?php echo $data['id']; ?>">Delete</a>
+
+                        </td>
+
+
+                    </tr>
                 <?php } ?>
             </tbody>
         </table>
@@ -83,5 +134,6 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     -->
-  </body>
+</body>
+
 </html>
